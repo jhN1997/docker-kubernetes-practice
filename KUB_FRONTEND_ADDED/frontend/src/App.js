fb4bb8120922da465f6a767a68f420cba3406440
John Chunqui Almeyda <johncalmeyda@gmail.com>
2025-09-30 02:27:10 -0500

    first commit

---
+++ KUB_FRONTEND_ADDED/frontend/src/App.js
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useState } from "react";
+/* eslint-disable react/react-in-jsx-scope */
+
+import "./App.css";
+import NewTask from "./components/NewTask";
+import TaskList from "./components/TaskList";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = useCallback(function () {
+    fetch("/api/tasks", {
+      headers: {
+        Authorization: "Bearer abc",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        setTasks(jsonData.tasks);
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      fetchTasks();
+    },
+    [fetchTasks]
+  );
+
+  function addTaskHandler(task) {
+    fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer abc",
+      },
+      body: JSON.stringify(task),
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (resData) {
+        console.log(resData);
+      });
+  }
+
+  return (
+    <div className="App">
+      <section>
+        <NewTask onAddTask={addTaskHandler} />
+      </section>
+      <section>
+        <button onClick={fetchTasks}>Fetch Tasks</button>
+        <TaskList tasks={tasks} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
